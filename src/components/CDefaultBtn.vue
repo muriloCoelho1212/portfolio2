5ce5f548beee879635e44ada0a01a634f80b3828
Murilo Coelho <m.coelhos1212@gmail.com>
2024-03-24 02:40:12 -0300

    feat: start the app and setup scss variables and prettier

---
+++ src/components/CDefaultBtn.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  const props = withDefaults(
+    defineProps<{
+      label?: string;
+      icon?: string;
+      tooltip?: string;
+      dense?: boolean;
+    }>(),
+    {
+      label: undefined,
+      tooltip: undefined,
+      icon: undefined,
+      dense: false,
+    },
+  );
+
+  const emit = defineEmits(['on-click']);
+</script>
+
+<template>
+  <q-btn
+    flat
+    :label="props.label"
+    :icon="props.icon"
+    :dense="props.dense"
+    @click="emit('on-click')"
+  >
+    <q-tooltip v-if="props.tooltip">{{ props.tooltip }}</q-tooltip>
+  </q-btn>
+</template>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header
+      class="bg-secondary"
+      bordered
+    >
+      <q-toolbar>
+        <q-toolbar-title> Murilo Coelho </q-toolbar-title>
+
+        <div class="row q-gutter-x-md">
+          <c-default-btn
+            label="Projetos"
+            dense
+          />
+          <c-default-btn
+            label="Formação"
+            dense
+          />
+          <q-separator
+            vertical
+            class="bg-white"
+          />
+          <c-default-btn
+            icon="fa-brands fa-github"
+            dense
+          />
+          <c-default-btn
+            icon="fa-brands fa-linkedin"
+            dense
+          />
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container class="bg-primary">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+  import CDefaultBtn from 'src/components/CDefaultBtn.vue';
+</script>
